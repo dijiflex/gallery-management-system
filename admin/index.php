fb19daae7cdad7a4675fe085599c3e200cec9d13
dijiflex <dijiflex@gmail.com>
2019-07-22 12:18:17 +0300

    Created Connections Beetween The Files

---
+++ admin/index.php
@@ -0,0 +1,24 @@
+<?php include("includes/header.php"); ?>
+
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    
+    <!-- Top Menu Items -->
+
+    <?php include("includes/topnav.php"); ?>
+
+    <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+
+    <?php include("includes/side_nav.php"); ?>
+    <!-- /.navbar-collapse -->
+</nav>
+
+<div id="page-wrapper">
+
+    <?php include("includes/admin_content.php"); ?>
+    <!-- /.container-fluid -->
+
+</div>
+<!-- /#page-wrapper -->
+
+<?php include("includes/footer.php");
---
+++ admin/upload.php
@@ -0,0 +1,53 @@
+<?php include("includes/header.php"); ?>
+
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.html">SB Admin</a>
+    </div>
+    <!-- Top Menu Items -->
+
+    <?php include("includes/topnav.php"); ?>
+
+    <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+
+    <?php include("includes/side_nav.php"); ?>
+    <!-- /.navbar-collapse -->
+</nav>
+
+<div id="page-wrapper">
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    UPLOAD
+                    <small>Subheading</small>
+                </h1>
+                <ol class="breadcrumb">
+                    <li>
+                        <i class="fa fa-dashboard"></i> <a href="index.html">Dashboard</a>
+                    </li>
+                    <li class="active">
+                        <i class="fa fa-file"></i> Blank Page
+                    </li>
+                </ol>
+            </div>
+        </div>
+        <!-- /.row -->
+
+    </div>
+
+   
+</div>
+<!-- /#page-wrapper -->
+
+<?php include("includes/footer.php");
